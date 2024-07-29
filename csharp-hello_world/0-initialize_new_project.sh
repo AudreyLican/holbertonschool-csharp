209217dc2ea1396ccef276dd2b1b0e4e35d69a73
AudreyLican <audrey.lican@gmail.com>
2024-07-28 23:57:13 -0400

    First commit

---
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# set project directory name
+PROJECT_DIR ="0-new_project"
+
+# Create the project directory if it doesn't exist
+if [ ! -d "$PROJECT_DIR"]; then
+    mkdir "$PROJECT_DIR"
+    echo "Directory '$PROJECT_DIR' created."
+else
+    echo "Directory '$PROJECT_DIR' already exists."
+
+fi
+
+# navigate into the project directory
+cd "$PROJECT_DIR"
+
+# Initialize a new C# console project
+dotnet new console
