1184572452bac9a69fd62da059da648601c29dfe
AudreyLican <audrey.lican@gmail.com>
2024-07-30 07:10:45 -0400

    added namespace

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,9 +1,11 @@
 using System;
 
+namespace writeline
+{
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("\"Programming is like building a multilingual puzzle");
     }
-}
+}}
