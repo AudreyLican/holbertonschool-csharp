5001514a300023dee76e7531300df08bcb128d24
AudreyLican <audrey.lican@gmail.com>
2024-07-30 06:43:02 -0400

    task 12

---
+++ csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+        Console.Error.WriteLine();
+        Environment.ExitCode = 1;
+    }
+}
