f7eab9a3267faecd98f4fa46a26107a9d4cc9159
AudreyLican <audrey.lican@gmail.com>
2024-07-31 11:55:47 -0400

    update

--- csharp-hello_world/3-writeline/obj/Debug/netcoreapp2.1/3-writeline.AssemblyInfo.cs
+++ csharp-hello_world/3-writeline/obj/Debug/netcoreapp2.1/3-writeline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9009546d306585994966fa7b87dad161250ca462")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d7f1cd5a35bf702fb1ee7a37d8a35fa209b8142")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-writeline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
