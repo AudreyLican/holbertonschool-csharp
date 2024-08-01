0635666f763698aec7c59578aa71207ee46683d0
AudreyLican <audrey.lican@gmail.com>
2024-08-01 11:42:33 -0400

    task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+        {
+            int nbr1 = i % 10;
+            int nbr2 = i / 10;
+
+            if (nbr1 != nbr2 && nbr2 < nbr1)
+                {
+                    if (i != 89)
+                        Console.Write("{0:D2}, ", i);
+                    else
+                        Console.Write("{0:D2}", i);
+                }
+        }
+    }
+}
