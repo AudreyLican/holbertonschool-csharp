396ae29dd2d76d9b562ba6dfa11c2a6655876366
AudreyLican <audrey.lican@gmail.com>
2024-08-01 12:08:18 -0400

    task 8

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int isLastdigit = number % 10;
+        if (isLastdigit < 0)
+            isLastdigit *= -1;
+        Console.Write(isLastdigit);
+
+        return isLastdigit;
+    }
+}
