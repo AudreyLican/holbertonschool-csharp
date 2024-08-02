fb901807af0eb5e887c8d3c308306cd1ea41105b
AudreyLican <audrey.lican@gmail.com>
2024-08-02 19:58:08 -0400

    task 0

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] numArray = new int[size];
+
+        if (size == 0)
+            {
+                Console.WriteLine("");
+                return numArray;
+            }
+
+        for (int i = 0; i < size; i++)
+        {
+            numArray[i] = i; 
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+        return numArray;
+    }
+}
