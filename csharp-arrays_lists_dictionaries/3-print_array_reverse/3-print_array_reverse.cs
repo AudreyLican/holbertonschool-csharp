8ce7c7f0a33ada9648d7d30e5a2586eed0daff6c
AudreyLican <audrey.lican@gmail.com>
2024-08-04 12:57:35 -0400

    check null none

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,18 +4,11 @@ class Array
 {
     public static void Reverse(int[] array)
     {
-        int i;
-
-        if (array == null || array.Length == 0)
-            Console.WriteLine();
-        else
+        for (i = array.Length - 1; i >= 0; i--)
         {
-            for ( i = array.Length - 1; i >= 0; i--)
-            {
-                Console.Write(array[i]);
-                if (i > 0)
-                    Console.Write(" ");
-            }
+            Console.Write(array[i]);
+            if (i > 0)
+                Console.Write(" ");
         }
         Console.WriteLine();
     }
