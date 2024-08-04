0458fc7f61f72661827a7cdbb49516465652e58e
AudreyLican <audrey.lican@gmail.com>
2024-08-04 08:52:15 -0400

    task 6

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> isDivBy2 = new List<bool>();
+
+        foreach (int i in myList)
+        {
+            if (i % 2 == 0)
+                isDivBy2.Add(true);
+            else
+                isDivBy2.Add(false);
+        }
+        return isDivBy2;
+    }
+}
