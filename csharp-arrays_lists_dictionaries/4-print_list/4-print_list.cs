53de845687ca3f758513ba0dcc47fea6d76cacda
AudreyLican <audrey.lican@gmail.com>
2024-08-04 08:14:21 -0400

    task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        // Create a List of integers
+        List<int> newList = new List<int>();
+
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return newList;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write("{0}{1}", i, i != size - 1 ? " ": "" );
+            newList.Add(i);
+        }
+        Console.WriteLine();
+        return newList;
+    }
+}
