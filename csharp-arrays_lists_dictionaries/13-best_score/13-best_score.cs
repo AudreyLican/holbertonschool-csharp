b5cb3c5af686e5fd6da3524c0efb4f8097a6d9e4
AudreyLican <audrey.lican@gmail.com>
2024-08-04 16:33:33 -0400

    task 13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+            return "None";
+
+        string highestScore = "";
+        int biggestInt = 0;
+
+        foreach (KeyValuePair<string, int> i in myList)
+        {
+            if (i.Value >= biggestInt)
+            {
+                biggestInt = i.Value;
+                highestScore = i.Key;
+            }
+        }
+
+        return highestScore;
+    }
+}
