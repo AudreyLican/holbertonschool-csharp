e00a287b219e57fadda62b617400c827f0feba41
AudreyLican <audrey.lican@gmail.com>
2024-08-04 08:13:47 -0400

    update

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -14,10 +14,10 @@ class Array
         int[] numArray = new int[size];
 
         if (size == 0)
-            {
-                Console.WriteLine("");
-                return numArray;
-            }
+        {
+            Console.WriteLine("");
+            return numArray;
+        }
 
         for (int i = 0; i < size; i++)
         {
