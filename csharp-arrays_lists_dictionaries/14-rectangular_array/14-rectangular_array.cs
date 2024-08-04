579cf6eec718a095d6619e21f31d93e27915e952
AudreyLican <audrey.lican@gmail.com>
2024-08-04 17:26:04 -0400

    task 12, 13, 14

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // create a 5 by 5 array
+        int[,] numArray = new int[5, 5];
+
+        // initialize all to 0
+        for (int i= 0; i < 5; i++)
+        {
+            for (int j=0; j < 5; j++)
+                numArray[i, j] = 0;
+        }
+
+        // set index to [2, 2] to 1
+        numArray[2, 2] = 1;
+
+        // print array
+        for (int i= 0; i < 5; i++)
+        {
+            for (int j=0; j < 5; j++)
+                Console.Write(numArray[i, j] + " ");
+            Console.WriteLine();
+        }
+    }
+}
