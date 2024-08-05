2e4c20291fecff558f6466947e21aedf23aca634
AudreyLican <audrey.lican@gmail.com>
2024-08-05 07:01:49 -0400

    task 2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+    Write a method that returns a sorted list of all elements present in one or the other list but not both.
+    Returns a new sorted list of all elements present in one or the other list but not both of the given lists
+*/
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> newlist1 = new List<int>();
+        List<int> newlist2 = new List<int>();
+
+        // find unique element in list 1
+        foreach (int element in list1)
+        {
+            if (!list2.Contains(element))
+                newlist1.Add(element);
+        }
+
+        // find unique elment in list 2
+        foreach (int element in list2)
+        {
+            if (!list1.Contains(element))
+                newlist2.Add(element);
+        }
+
+        // combine list 1 & 2
+        List<int> diffEl = new List<int>();
+        diffEl.AddRange(newlist1);
+        diffEl.AddRange(newlist2);
+
+        //sort the combined list
+        diffEl.Sort();
+
+        return diffEl;
+    }
+}
