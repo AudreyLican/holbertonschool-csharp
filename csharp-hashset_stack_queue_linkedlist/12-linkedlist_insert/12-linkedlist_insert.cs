8702d2910ba8e74d329013d9284c8167ce01a164
AudreyLican <audrey.lican@gmail.com>
2024-08-05 19:11:49 -0400

    task 12

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> currentNode = myLList.First;
+
+        while (currentNode != null)
+        {
+            if (currentNode.Value < n)
+                currentNode = currentNode.Next;
+            else
+            {
+                myLList.AddBefore(currentNode, n);
+                return myLList.First;
+            }
+        }
+        myLList.AddLast(n);
+        return myLList.First;
+    }
+}
