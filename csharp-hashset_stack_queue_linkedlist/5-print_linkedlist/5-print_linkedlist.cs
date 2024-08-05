13acf62c6f85ccd40cd403da19216eb20016f29e
AudreyLican <audrey.lican@gmail.com>
2024-08-05 11:25:35 -0400

    task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+    Write a method that creates and prints a LinkedList of integers of a given size.
+*/
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newLList = new LinkedList<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            newLList.AddLast(i);
+            Console.WriteLine(i);
+        }
+
+        return newLList;
+    }
+}
