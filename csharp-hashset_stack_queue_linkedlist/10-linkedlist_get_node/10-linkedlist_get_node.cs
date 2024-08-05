5ee5a2f9e20773f1e3fd1038a772a6cb573b1624
AudreyLican <audrey.lican@gmail.com>
2024-08-05 13:38:37 -0400

    task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    // Main - entry point
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (myLList.Count < n)
+            return 0;
+
+        int index = 0;
+        foreach(int j in myLList)
+        {
+            if (index == n)
+                return j;
+            index++;
+        }
+
+        return 0;
+    }
+}
