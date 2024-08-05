a4333acf48d1efb4d6a0605ada753646d76858f0
AudreyLican <audrey.lican@gmail.com>
2024-08-04 21:50:37 -0400

    task 1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+    Write a method that returns a sorted list of common elements in two lists.
+    return a new sorted list of common elements in two given lists
+*/
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonEl = new List<int>();
+
+        foreach (int element in list1)
+        {
+            if (list2.Contains(element) && !commonEl.Contains(element))
+                commonEl.Add(element);
+        }
+
+        return commonEl;
+    }
+}
