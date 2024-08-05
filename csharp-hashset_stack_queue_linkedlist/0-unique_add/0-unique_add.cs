c01d96a0a436cb4801c12abc7d84828755b41012
AudreyLican <audrey.lican@gmail.com>
2024-08-04 21:12:02 -0400

    task 0

--- csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -5,6 +5,14 @@ class List
 {
     public static int Sum(List<int> myList)
     {
-        
+        // create hashset
+        HashSet<int> uqInt = new HashSet<int>(myList);
+
+        int sum = 0;
+
+        foreach (int nbr in uqInt)
+            sum += nbr;
+
+        return sum;
     }
 }
