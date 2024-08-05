cf6b60bd17d700418ca03902ce44caf5777e52b4
AudreyLican <audrey.lican@gmail.com>
2024-08-05 11:30:55 -0400

    task 6

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    // Main - entry point
+    public static int Length(LinkedList<int> myLList)
+    {
+        int lenList = 0;
+        foreach (int i in myLList)
+            lenList += 1;
+
+        return lenList;
+    }
+}
