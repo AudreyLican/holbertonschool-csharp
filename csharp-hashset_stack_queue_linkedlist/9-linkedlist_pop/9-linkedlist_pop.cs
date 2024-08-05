23ef2b005f51563fad99f72e5cb7219f1049b669
AudreyLican <audrey.lican@gmail.com>
2024-08-05 12:06:35 -0400

    task 9

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    // Main - entry point
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count <= 0)
+            return 0;
+
+        int f = myLList.First.Value; // get the value of the first node
+        myLList.RemoveFirst();
+
+        return f;
+    }
+}
