95d93c69b1abcb98ae68685626b26e31cff40611
AudreyLican <audrey.lican@gmail.com>
2024-08-05 18:26:45 -0400

    task 11

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+    Write a method that returns the sum of all the data of a LinkedList.
+    Returns the sum of the data in the LinkedList.
+*/
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+
+        foreach(int i in myLList)
+        {
+            sum += i;
+        }
+
+        return sum;
+    }
+}
