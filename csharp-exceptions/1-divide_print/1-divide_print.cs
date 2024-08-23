fcaab801cd89c8ba47675690beb75f4633ccf7e4
AudreyLican <audrey.lican@gmail.com>
2024-08-23 06:53:11 -0400

    task 1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int sum = 0;
+        try
+        {
+            sum = a / b;
+        }
+        catch
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, sum);
+        }
+    }
+}
