b9bfd2b840279fbace92fd3809cc05b82d6de5a3
AudreyLican <audrey.lican@gmail.com>
2024-08-29 17:04:24 -0400

    task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// public class Zombie, defines a zombie.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>
+        /// Health of the zombie.
+        /// </summary>
+        public int health;
+
+    /// <summary>
+    /// public constructor Zombie, health of zombie.
+    /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
