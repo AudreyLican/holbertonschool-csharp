2298d5c677ea936d1ba64eec8a2709c863b6c045
AudreyLican <audrey.lican@gmail.com>
2024-08-29 19:09:29 -0400

    task 5

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// public class Zombie, defines a zombie.
+    /// </summary>
+    class Zombie
+    {
+        /// <summary>
+        /// Health of the zombie.
+        /// </summary>
+        private int health;
+        private string name = "(No name)";
+
+        /// <summary>
+        /// public constructor Zombie, health of zombie.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// public constructor Zombie
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = value;
+        }
+
+        /// <summary>
+        /// public method of Zombie Health
+        /// </summary>
+        /// <returns> Return Zombie Health </returns>
+        public int GetHealth()
+        {
+            return (health);
+        }
+
+        /// <summary>
+        /// public property
+        /// </summary>
+        public string Name
+        {
+            get {return name;}
+            set {name = value;}
+        }
+
+        /// <summary>
+        /// public method override Zombie Object's attribut to stdout.
+        /// </summary>
+        /// <returns> new string </returns>
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
+    }
+}
