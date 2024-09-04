40e7df4f967b27c497124ef86f812ba681aca4cd
AudreyLican <audrey.lican@gmail.com>
2024-09-04 11:19:14 -0400

    added comments

--- csharp-inheritance/1-instance/1-instance.cs
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 class Obj
 {
+    /// <summary>
+    /// method that verify if object is instance of type Array
+    /// </summary>
+    /// <param name="obj">object to check</param>
+    /// <returns>True is instance id=f of type Array, or False if not</returns>
     public static bool IsInstanceOfArray(object obj)
     {
         return obj is Array;
