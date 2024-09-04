a58babdfb7c62ed273a68fe4c4ece642fd78fa36
AudreyLican <audrey.lican@gmail.com>
2024-09-04 11:02:54 -0400

    task 0

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    /// <summary>
+    /// Check if number is a int
+    /// </summary>
+    /// <param name="obj">Element to check</param>
+    /// <returns>True if int or false if not</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
