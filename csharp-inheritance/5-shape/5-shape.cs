54a119d4b5aeebe084dd321827ab728d71ce614a
AudreyLican <audrey.lican@gmail.com>
2024-09-05 12:04:23 -0400

    task 5

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>class Shape</summary>
+class Shape
+{
+    /// <summary>
+    /// virtual method Area that throw a NotImplementedException
+    /// </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
