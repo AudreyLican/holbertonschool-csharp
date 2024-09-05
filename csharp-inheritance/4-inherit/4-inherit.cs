8104a61ef46fce950f65ee6d77a3476d83f2bdb8
AudreyLican <audrey.lican@gmail.com>
2024-09-05 11:50:47 -0400

    task 4

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Base class Animal
+/// </summary>
+class Animal
+{
+}
+
+/// <summary>
+/// Derived class- subclass Dog
+/// </summary>
+class Dog : Animal
+{
+}
