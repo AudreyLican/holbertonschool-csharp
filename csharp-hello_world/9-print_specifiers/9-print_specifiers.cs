fc9d12ca075cf7748af5e6c36d1843cbff81cf86
AudreyLican <audrey.lican@gmail.com>
2024-12-08 19:32:44 +0100

    update answer string currency

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@ class Program
 		double percent = .7553;
 		double currency = 98765.4321;
 		Console.WriteLine("Percent: {0:P2}", percent);
-        Console.WriteLine("Currency: " + currency.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+        Console.WriteLine($"Currency: {currency.ToString("C", System.Globalization.CultureInfo.GetCultureInfo("en-US"))}");
 	}
 }
