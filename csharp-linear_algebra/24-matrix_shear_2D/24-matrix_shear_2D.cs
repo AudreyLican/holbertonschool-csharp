e12dd2974e869e8378cd2052c0b4e0201aa0cfbd
AudreyLican <audrey.lican@gmail.com>
2025-02-02 22:36:22 +0100

    task 22

---
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,46 @@
+﻿using System;
+/// <summary>
+/// class matrix math
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// method that shears a square 2D matrix by a given shear factor and returns the resulting matrix.
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <param name="direction"></param>
+    /// <param name="factor"></param>
+    /// <returns></returns>
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        // Check if the matrix is square
+        if (matrix.GetLength(0) != matrix.GetLength(1))
+            return new double[,] { { -1 } };
+
+        int size = matrix.GetLength(0);
+        double[,] result = new double[size, size];
+
+        // Apply the shear transformation based on the direction
+        if (direction == 'x' || direction == 'X')
+        {
+            // Horizontal shear (shear in the X direction)
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    result[i, j] = matrix[i, j] + factor * i; // Apply the shear transformation in the X direction
+        }
+        else if (direction == 'y' || direction == 'Y')
+        {
+            // Vertical shear (shear in the Y direction)
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    result[i, j] = matrix[i, j] + factor * j; // Apply the shear transformation in the Y direction
+        }
+        else
+        {
+            // Invalid direction, return matrix with -1
+            return new double[,] { { -1 } };
+        }
+
+        return result;
+    }
+}
