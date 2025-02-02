060207a01a891207fe6f2077e4cca11434caea63
AudreyLican <audrey.lican@gmail.com>
2025-02-02 21:01:22 +0100

    task 12

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+/// <summary>
+/// class Vector math
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// calculate dot product of either tw0 2D or two 3D vectors.
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns></returns>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        // checking if vector is 2D or 3D
+        if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length < 3
+        || vector2.Length != 2 || vector2.Length != 3)
+            return -1;
+
+        double[] result = new double[vector1.Length];
+
+        for (int i = 0; i < vector1.Length; i++)
+            result[i] += vector1[i] * vector2[i];
+
+        return result;
+    }
+}
