a31913ff08f8fd423bb84d4cf849c2d59c2c7464
AudreyLican <audrey.lican@gmail.com>
2025-02-02 22:47:02 +0100

    task 26

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -11,27 +11,21 @@ class MatrixMath
     /// <returns></returns>
     public static double Determinant(double[,] matrix)
     {
-        // Check if the matrix is 2x2 or 3x3
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
+        int matrixRows = matrix.GetLength(0);
+        int matrixCols = matrix.GetLength(1);
 
-        if (rows != cols || (rows != 2 && rows != 3))
-            return -1; // If the matrix is not 2x2 or 3x3, return -1
+        if (matrixRows == 2 && matrixCols == 2)
+            return Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
 
-            // If it's a 2x2 matrix, use the 2x2 determinant formula
-        if (rows == 2)
-            return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
-
-        // If it's a 3x3 matrix, use the 3x3 determinant formula (Cofactor Expansion)
-        if (rows == 3)
+        else if (matrixRows == 3 && matrixCols == 3)
         {
             double a = matrix[0, 0], b = matrix[0, 1], c = matrix[0, 2];
             double d = matrix[1, 0], e = matrix[1, 1], f = matrix[1, 2];
             double g = matrix[2, 0], h = matrix[2, 1], i = matrix[2, 2];
 
-            return a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
+            return Math.Round((a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g)), 2);
         }
 
-        return -1; // Fallback in case of invalid input (shouldn't be reached due to earlier check)
+        return -1; // Fallback in case of invalid input
     }
 }
