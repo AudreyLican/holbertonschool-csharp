523db3b7001d8c1d9c93e263a9802a8684c7acd2
AudreyLican <audrey.lican@gmail.com>
2025-02-02 20:46:14 +0100

    task 6

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -14,9 +14,7 @@ public class VectorMath
         double[] result = new double[vector1.Length];
 
         for (int i = 0; i < vector1.Length; i++)
-        {
             result[i] = vector1[i] + vector2[i];
-        }
 
         return result;
     }
