f69a052dc25ce058e044accd27fc63ffd220397a
AudreyLican <audrey.lican@gmail.com>
2025-02-02 20:33:18 +0100

    added documentation

--- csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -1,7 +1,15 @@
 ﻿using System;
-
+/// <summary>
+/// class Vector Math
+/// </summary>
 class VectorMath
 {
+    /// <summary>
+    /// method that multiplies a vector and a scalar
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <param name="scalar"></param>
+    /// <returns>return the resulting vector</returns>
     public static double[] Multiply(double[] vector, double scalar)
     {
         // check if vector is 2D or 3D
