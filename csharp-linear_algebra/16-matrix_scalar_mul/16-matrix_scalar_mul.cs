ba068a3a48ca55b3124aaabcd3dff364d1d42617
AudreyLican <audrey.lican@gmail.com>
2025-02-02 22:21:29 +0100

    change if condition

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -13,18 +13,15 @@ class MatrixMath
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
         //check if matrix is 2D or 3D
-        if (matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3)
+        if ((matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3) || (matrix.GetLength(1) != 2 && matrix.GetLength(1) != 3))
             return new double[,] { {-1}};
 
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-
         // Create a new matrix to store the result of multiplicatio
-        double[,] result = new double[rows, cols];
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
 
         // Multiply each element of the matrix by the scalar
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
                 result[i, j] = matrix[i, j] * scalar;
 
         return result;
