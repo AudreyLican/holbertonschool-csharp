f136c772bc90a9a05a9bcc3992d284df5ba7cc99
AudreyLican <audrey.lican@gmail.com>
2025-02-02 21:37:34 +0100

    task 14

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -13,9 +13,9 @@ class MatrixMath
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
         // Check if both matrices are 2D or 3D and if dimensions match
-        if ((matrix1.length != matrix2.length) || (matrix1[0].length != matrix2[0].length) ||
-            (matrix1.length != 2 && matrix1.length != 3 || matrix2.length != 2 && matrix2.length != 3))
-            return new double[,] { {-1}};
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1) ||
+            (matrix1.GetLength(0) != 2 && matrix1.GetLength(0) != 3) || (matrix2.GetLength(0) != 2 && matrix2.GetLength(0) != 3))
+            return new double[,] { { -1 } };
 
         int rows = matrix1.GetLength(0);
         int cols = matrix1.GetLength(1);
