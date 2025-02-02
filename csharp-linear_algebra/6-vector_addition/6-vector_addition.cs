3a721c42e2357e9995ed13784f3b5dead0af8028
AudreyLican <audrey.lican@gmail.com>
2025-02-02 20:04:37 +0100

    change if condition usiing or

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,17 +1,25 @@
 ﻿using system;
 
+/// <summary>
+/// Class method vector math
+/// </summary>
 public class VectorMath
 {
+    /// <summary>
+    /// Adds 2 vectors and return the resulting vector
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns>resulting vector btw vector1 and vector2</returns>
     public static double[] Add(double[] vector1, double[] vector2)
     {
         //verify if vector is 2D or 3D
-        if (vector1.Length != vector2.Length || vector1.Length != 2 || vector1.Length != 3
-        || vector2.Length != 2 || vector2.Length != 3)
+        if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3
+        || vector2.Length < 2 || vector2.Length > 3)
         {
             return new double[] { -1 };
         }
 
-        //add component
         double[] result = new double[vector1.Length];
 
         for (int i = 0; i < vector1.Length; i++)
@@ -22,4 +30,3 @@ public class VectorMath
         return result;
     }
 }
-//still looking for mistake
