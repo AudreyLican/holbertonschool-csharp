18077abeb8f011fa84fba41bdc6aaa6833a321ae
AudreyLican <audrey.lican@gmail.com>
2025-02-02 20:27:26 +0100

    task 9

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,19 @@
+﻿using system;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        // check if vector is 2D or 3D
+        if (vector.Length != 2 && vector.Length != 3)
+            return new double[] {-1};
+
+        double result = new double[vector.Length];
+
+        // multiply each element of the vector by the scalar
+        for (int i = 0; i < vector.Length; i++)
+            result[i] = vector[i] * scalar;
+
+        return result;
+    }
+}
