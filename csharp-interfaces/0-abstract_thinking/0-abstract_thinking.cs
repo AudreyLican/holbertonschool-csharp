876522ef22fc4715dd296cd29fc186144fa82be4
AudreyLican <audrey.lican@gmail.com>
2025-03-30 15:46:33 +0200

    Add task

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+/// <summary> Class : Base. </summary>
+public abstract class Base
+{
+    /// <summary> Name of the object. </summary>
+    public string name{get; set; };
+
+    /// <summary> Override string </summary>
+    /// <returns>formatted string </returns>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType()}";
+    }
+}
