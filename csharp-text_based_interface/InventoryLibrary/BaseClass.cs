3973a9df500e2b5b221939b1de159a5678a5d004
AudreyLican <audrey.lican@gmail.com>
2025-04-09 00:48:52 +0200

    first commit

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        public string id { get; set; }
+        public DateTime date_created { get; set; }
+        public DateTime date_updated { get; set; }
+
+        public BaseClass()
+        {
+            id = Guid.NewGuid().ToString();
+            date_created = new DateTime();
+            date_updated = new DateTime();
+        }
+    }
+}
